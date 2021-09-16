62f6afdf1770240109789ab737a5a0ae44fdd5d9
Cheemakurthi <Vasu.Cheemakurthi@cdk.com>
2021-09-16 10:45:20 +0530

    changed file again

--- sp_dim_group_1.sql
+++ sp_dim_group_1.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE edw.sp_dim_group3(in_src_last_modified "TIMESTAMP",in_run_id "int4",in_job_name "varchar") 
+CREATE OR REPLACE PROCEDURE edw.sp_dim_group1(in_src_last_modified "TIMESTAMP",in_run_id "int4",in_job_name "varchar") 
 LANGUAGE plpgsql AS $$
 
 DECLARE
